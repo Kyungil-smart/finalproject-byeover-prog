48aaf445c889334b16b44b2610e316ecf0e3a858
abcbbq99 <kim3373613@gmail.com>
2026-05-29 20:28:35 +0900

    Update DummyCombatTester.cs

--- AprilLog/Assets/_Game/_DevTest/DummyCombatTester.cs
+++ AprilLog/Assets/_Game/_DevTest/DummyCombatTester.cs
@@ -119,6 +119,7 @@ public class DummyCombatTester : MonoBehaviour
     {
         for (int i = 0; i < SortModel.UNIT_TYPE_COUNT; i++)
         {
+            // 유닛 타입별로 데미지를 다르게 줘서 발사 차이를 눈으로 구분한다.
             var data = new Legacy_SkillData
             {
                 SkillID = 9000 + i,
