c97e146d56c675bf66c8a3eae0a22616c3d27f91
DHchoi <dftyjk64@gmail.com>
2026-06-24 17:36:11 +0900

    조커블록 폴리싱

--- AprilLog/Assets/_Game/Features/Sort/JokerSystem.cs
+++ AprilLog/Assets/_Game/Features/Sort/JokerSystem.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
 using System.Collections.Generic;
+using UnityEngine.UI;
+
 
 public class JokerSystem : MonoBehaviour, IPointerClickHandler
 {
@@ -9,15 +11,19 @@ public class JokerSystem : MonoBehaviour, IPointerClickHandler
 
     [SerializeField] private SortInputHandler _inputHandler;
     [SerializeField] private JokerPatternLibrary _patternLibrary;
-    [SerializeField] private GameObject _jokerClonePrefab;
     [SerializeField] private List<Transform> _tableParents;
     [SerializeField] private RectTransform _effectSprite;
     [SerializeField] private SortModel _model;
     [SerializeField] private SortSystem _sortSystem;
     [SerializeField] private CanvasGroup[] _tableCanvasGroups;
+    [SerializeField] private SortTableView _view;
+    [SerializeField] private Image[] _jokerIcons;
 
     private JokerPatternData _activePattern;
     private int _currentIndex = 0;
+    private float _lastUsedTime = -60f;
+    private const float _coolDown = 60f;
+    private int _currentActiveIndex = 1; // 조커 몬스터 완성시 삭제 예정
 
     public void OnPointerClick(PointerEventData eventData)
     {
@@ -26,8 +32,21 @@ public class JokerSystem : MonoBehaviour, IPointerClickHandler
 
     public void ActivateJoker()
     {
+        if (Time.time - _lastUsedTime < _coolDown)
+        {
+            Debug.Log("쿨타임 중입니다!");
+            return;
+        }
+
         if (IsActive || _patternLibrary == null || _patternLibrary.patterns.Count == 0) return;
 
+        if (_currentActiveIndex >= 0)
+        {
+            _jokerIcons[_currentActiveIndex].enabled = false;
+            _currentActiveIndex--;
+        }
+
+        _lastUsedTime = Time.time;
         _activePattern = _patternLibrary.patterns[Random.Range(0, _patternLibrary.patterns.Count)];
         _currentIndex = 0;
 
@@ -56,7 +75,7 @@ public class JokerSystem : MonoBehaviour, IPointerClickHandler
 
     private IEnumerator JokerRoutine(int baseUnitType)
     {
-        JokerManager.Instance?.SetJokerActive(true);
+        if (_view != null) _view._isHintBlocked = true;
 
         IsActive = true;
         if (_inputHandler != null) _inputHandler.enabled = false;
@@ -64,21 +83,15 @@ public class JokerSystem : MonoBehaviour, IPointerClickHandler
 
         Debug.Log("조커 시스템 작동 시작");
 
-        if (_effectSprite != null) _effectSprite.gameObject.SetActive(true);
-
         for (int i = 0; i < 12; i++)
         {
             int targetTable = GetNextJokerTarget();
+
             HighlightTable(targetTable);
+
             int currentTableColor = FindFirstValidUnitInTable(targetTable);
             Debug.Log($"[조커 연출] {i + 1}회차: 타겟 테이블 {targetTable}");
             _model.ReplaceTableUnits(targetTable, currentTableColor);
-
-            GameObject clone = Instantiate(_jokerClonePrefab, _effectSprite);
-            clone.transform.position = _tableParents[targetTable].position;
-
-            var img = clone.GetComponent<UnityEngine.UI.Image>();
-            if (img != null) img.enabled = false;
 
             if (_model.IsTableMatched(targetTable))
             {
@@ -86,17 +99,15 @@ public class JokerSystem : MonoBehaviour, IPointerClickHandler
             }
 
             yield return new WaitForSeconds(0.3f);
-
-            Destroy(clone);
         }
 
         if (_effectSprite != null) _effectSprite.gameObject.SetActive(false);
 
         Debug.Log("조커 시스템 작동 종료");
         if (_inputHandler != null) _inputHandler.enabled = true;
-        
+        if (_view != null) _view._isHintBlocked = false;
+
         IsActive = false;
-        JokerManager.Instance?.SetJokerActive(false);
     }
 
     private void HighlightTable(int targetIndex)
@@ -104,6 +115,15 @@ public class JokerSystem : MonoBehaviour, IPointerClickHandler
         for (int i = 0; i < _tableCanvasGroups.Length; i++)
         {
             _tableCanvasGroups[i].alpha = (i == targetIndex) ? 0f : 0.78f;
+        }
+    }
+
+    public void AcquireJokerItem()
+    {
+        if (_currentActiveIndex < _jokerIcons.Length - 1)
+        {
+            _currentActiveIndex++;
+            _jokerIcons[_currentActiveIndex].enabled = true;
         }
     }
 }
