f0f5b075abee18142b5220ab8f7b0d27b7ca4efd
DHchoi <dftyjk64@gmail.com>
2026-06-29 14:34:50 +0900

    엘리트 인챈트 보상 구현

--- AprilLog/Assets/Scenes/CDH/EliteMonsterReward.cs
+++ AprilLog/Assets/Scenes/CDH/EliteMonsterReward.cs
@@ -8,17 +8,33 @@ public class EliteMonsterReward : MonoBehaviour
 
     public void OpenEnchantPopup()
     {
-        if (_enchantView != null)
-        {
-            _enchantView.Show();
-        }
+        _enchantView.gameObject.SetActive(false);
+
+        StartCoroutine(ResetAndShow());
+    }
+
+    private System.Collections.IEnumerator ResetAndShow()
+    {
+        yield return null;
+
+        _enchantView.gameObject.SetActive(true);
+
+        _enchantView.Show();
+
+        Debug.Log("[Reward] 팝업 다시 호출 완료");
     }
 
     public void CloseEnchantPopup()
     {
+        Debug.Log($"[Reward] CloseEnchantPopup 호출됨. Manager 존재 여부: {Manager != null}");
         if (Manager != null)
         {
             Manager.ShowNextPopup();
+        }
+
+        else
+        {
+            Debug.LogError("[Reward] Manager가 null입니다! 연결을 확인하세요.");
         }
     }
 }
