5bda8093a728bc9a16db76be326af9a6916d3b28
DHchoi <dftyjk64@gmail.com>
2026-07-10 10:54:51 +0900

    인첸트 교체시 시간 미정지 버그 수정

--- AprilLog/Assets/Scenes/CDH/EnchantPopupManager.cs
+++ AprilLog/Assets/Scenes/CDH/EnchantPopupManager.cs
@@ -28,13 +28,14 @@ public class EnchantPopupManager : MonoBehaviour
 
     public void StartRewardSequence()
     {
-        float rand = Random.value;
+        /*float rand = Random.value;
         if (rand < 0.1f) _remainingPopups = 3;
         else if (rand < 0.4f) _remainingPopups = 2;
         else _remainingPopups = 1;
 
-        Debug.Log($"[EnchantPopup] 확률 결과: {rand:F2} (0~0.1: 10%, 0.1~0.4: 30%, 0.4~1.0: 60%) | 생성된 팝업 횟수: {_remainingPopups}");
+        Debug.Log($"[EnchantPopup] 확률 결과: {rand:F2} (0~0.1: 10%, 0.1~0.4: 30%, 0.4~1.0: 60%) | 생성된 팝업 횟수: {_remainingPopups}");*/
 
+        _remainingPopups = 3;
 
         if (_rewardEffect != null)
         {
